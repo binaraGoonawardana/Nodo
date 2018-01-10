f47adb301be2dce3cb8c25912398b6ab179e44dc
Binara Goonawardana <binara.is@live.com>
2018-01-10 18:58:54 +0530

    configured restify server

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const config = require('./config');
+const restify = require('restify'),
+  config = require('./config');
 
-console.log(process.env.NODE_ENV);
-console.log(config.port);
+const server = restify.createServer({
+  name: 'Nodo',
+  version: '1.0.0'
+});
+
+server.get('/echo/:name', (req, res, next) => {
+  res.send(req.params);
+  next();
+});
+
+server.listen(config.port, () => {
+  console.log('%s listening at %s', server.name, server.url);
+});
